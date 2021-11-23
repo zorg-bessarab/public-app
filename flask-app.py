7bb92439ef06d01825b15c5785900d1f8c40a95c
Michael Bessarab <MBessarab@ru.ibm.com>
2021-11-23 11:45:57 +0300

    Update flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     secret = os.getenv('AQUA_TEST')
     return f'<h1>Hello from Test Secret: {secret} </h1>'
-    
+
+
 if __name__ == "__main__":
     app.run()
