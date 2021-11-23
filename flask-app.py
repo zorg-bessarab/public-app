83c5148d8c3db53c8b33de9578588634eab2007e
Michael Bessarab <MBessarab@ru.ibm.com>
2021-11-23 11:11:28 +0300

    Update flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     secret = os.getenv('AQUA_TEST')
-    return f'<h1>Hello from Test Secret {secret} </h1>'
+    return f'<h1>Hello from Test Secret: {secret} </h1>'
     
 if __name__ == "__main__":
     app.run()
