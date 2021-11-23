7d3d1dbe518df483b648e84018c4cb5f79234f5e
Michael Bessarab <MBessarab@ru.ibm.com>
2021-11-23 11:03:06 +0300

    aqua-vault test added

--- flask-app.py
+++ flask-app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello from Flask</h1>'
+    return f'<h1>Hello from Test Secret {os.getenv('AQUA_TEST')}</h1>'
     
 if __name__ == "__main__":
     app.run()
