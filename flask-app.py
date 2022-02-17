01ad60430c0b3e2531469712db91687dabecba06
Michael Bessarab <MBessarab@ru.ibm.com>
2022-02-17 16:02:50 +0300

    pipeline change. delete sonar

--- flask-app.py
+++ flask-app.py
@@ -4,10 +4,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
+secret = os.environ['AQUA_TEST']
+
+
+@app.route(f'/{secret}')
+def secret():
+    return f'Secret is: {secret}'
+
+
 @app.route('/')
 def index():
-    secret = os.getenv('AQUA_TEST')
-    return f'<h1>Hello from Test Secret: {secret} </h1>'
+    return f'<h1>Hello from Test Secrer</h1>'
 
 
 if __name__ == "__main__":
